2a12c8231168f902fdbfb96edc1a3aa3ed7a17dc
Carlos Castillo Blas <carlosCACB333@gmail.com>
2022-10-17 16:07:06 -0500

    Listo para empezar el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import React from 'react';
-
 function App() {
-  return (
-    <div className="container mx-auto mt-4">
-      <header className="App-header">
-        <h1 className="text-4xl">blog post</h1>
-      </header>
-    </div>
-  );
+  return <div className="container mx-auto mt-4"></div>;
 }
 
 export default App;
