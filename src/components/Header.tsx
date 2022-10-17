2559cb00e064e588b0fbf2d5f6ce6d1cfb289e91
Carlos Castillo Blas <carlosCACB333@gmail.com>
2022-10-17 17:42:17 -0500

    Modifiando las urls desde header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,19 +15,13 @@ export const Header = () => {
 
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4"
-          >
+          <a href="#rasta" className="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">
             Docs
           </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4"
-          >
+          <a href="#rasta" className="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white mr-4">
             Examples
           </a>
-          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white">
+          <a href="#rasta" className="block mt-4 lg:inline-block lg:mt-0 text-indigo-200 hover:text-white">
             Blog
           </a>
         </div>
