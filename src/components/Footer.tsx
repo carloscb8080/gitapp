caf9a33c3eed6fbd8aa77701a36e48ce2ca57fdb
Carlos Castillo Blas <carloscb8080@gmail.com>
2022-10-18 16:39:53 -0500

    Edité el footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,22 @@
 // with tailwindcss
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-8">
-      <p>© 2021 - Bonny inc</p>
+    <footer className="bg-gray-800 text-indigo-100 text-center py-4 mt-8 flex justify-center gap-4">
+      <p>© 2021 - Bonny</p>
+
+      <p>
+        <a href="/" className="text-indigo-300 hover:text-indigo-200">
+          Home
+        </a>{" "}
+        |{" "}
+        <a href="/" className="text-indigo-300 hover:text-indigo-200">
+          About
+        </a>{" "}
+        |{" "}
+        <a href="/" className="text-indigo-300 hover:text-indigo-200">
+          Contact
+        </a>
+      </p>
     </footer>
   );
 };
