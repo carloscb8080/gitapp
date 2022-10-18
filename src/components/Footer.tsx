560b6b328385ea53180e15c379009e4b7cf46e0d
Carlos Antonio Castillo Blas <carloscacb333@gmail.com>
2022-10-17 23:14:19 -0500

    agregando cositas al footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 // with tailwindcss
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4">
-      <p>© 2021 - My Company</p>
+    <footer className="bg-gray-800 text-white text-center py-4 mt-8">
+      <p>© 2021 - Bonny inc</p>
     </footer>
   );
 };
