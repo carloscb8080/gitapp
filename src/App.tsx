5cf0388e5f3ff66c749d8478bef57f6027971ce0
Carlos Castillo Blas <carloscb8080@gmail.com>
2022-10-17 22:42:27 -0500

    agregando un salto de linea

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container mx-auto mt-4">
       <Header />
 
-      {/* posts list */}
+      <br />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((post) => (
           <CardPost key={post} />
