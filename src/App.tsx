f30d35b93ac1caa60c68991e6c505748e391fa91
Carlos Castillo Blas <carloscb8080@gmail.com>
2022-10-19 17:31:43 -0500

    retoques finales

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { CardPost, Footer, Header } from './components';
 
 function App() {
   return (
-    <div className="container mx-auto mt-4">
+    <div className="container mx-auto ">
       <Header />
 
       <br />
